b6ec793d76d07a807b05bf06bf3fe9fe45423ffa
Hector F. Jimenez <hfjimenez@utp.edu.co>
2016-09-29 21:39:42 -0500

    Convert Books

---
+++ HomeWorks/converterbook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#hfjimenez@utp.edu.co 
+#Purpouse: Delete all the accents, uppers to lowercase. 
+#Recomendations: Please remove the guttember prolog
+#usage ./converterbooks.sh book.txt filtered.txt
+file="$1"
+file2="$2"
+#cat $file 
+cat $file |tr -d [[:punct:]]> $file2                                    #Avoid modification to the original book.
+sed -i 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚñÑçÇ/aAaAaAaAeEeEiIoOoOoOuUnNcC/' $file2 #Delete the accent using Awk :)
+tr '[A-Z]' '[a-z]' < $file2
